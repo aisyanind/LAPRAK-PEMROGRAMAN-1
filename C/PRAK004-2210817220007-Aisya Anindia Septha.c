fa7d30d1b2cc9d0e9a4825d63745f539afb699e8
aisyanind <115249414+aisyanind@users.noreply.github.com>
2022-10-08 19:22:40 +0800

    Add files via upload

---
+++ C/PRAK004-2210817220007-Aisya Anindia Septha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int baris;
+for(baris = 0; baris < 29; baris++)
+{printf("#");}
+printf("\n#                           #\n");
+printf("#   Aisya Anindia Septha    #\n");
+printf("#      2210817220007        #\n");
+printf("#                           #\n");
+for(baris = 0; baris < 29; baris++)
+{printf("#");}
+}
