44a27364c484f35dc85ab773c6abfdde81dded21
aisyanind <115249414+aisyanind@users.noreply.github.com>
2022-10-08 19:23:43 +0800

    Add files via upload

---
+++ Python/PRAK004-2210817220007-Aisya Anindia Septha.py
@@ -0,0 +1,6 @@
+print("#"*29)
+print("#                           #");
+print("#   Aisya Anindia Septha    #");
+print("#       2210817220007       #");
+print("#                           #");
+print("#"*29)   
